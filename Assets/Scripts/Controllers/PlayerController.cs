6288f2522708742a9b26c168db12e687c702567e
SeokHyoung Lee <dltjrgud815@gmail.com>
2023-08-08 01:17:32 +0900

    Make Dyanamic Camera and Make a Touch Status,  New Folder(Utils)

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 struct MyVector
 {
@@ -55,10 +58,35 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 10.0f;
 
+    private bool _moveToDest = false;
+    private Vector3 _destPos;
+
     private void Start()
     {
         Managers.Input.KeyAction -= OnKeyBoard;
         Managers.Input.KeyAction += OnKeyBoard;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
+    }
+
+    private void Update()
+    {
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.00001f)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                transform.position += dir.normalized * moveDist;
+                
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+                transform.LookAt(_destPos);
+            }
+        }
     }
 
     private void OnKeyBoard()
@@ -87,6 +115,25 @@ public class PlayerController : MonoBehaviour
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.3f);
             transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+
+        _moveToDest = false;
+    }
+    
+    private void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click)
+            return;
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 10f, Color.red, 1.0f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _moveToDest = true;
+            //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
         }
     }
 }
